ad8a2dbc453d24fd4f00880a23f9ef0c9d6658bf
divanini <equiptrix@outlook.com>
2023-08-14 16:26:40 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.menu');
+const list = document.querySelector('.nav__links');
+const close = document.querySelector('.close');
+menu.onclick = function () {
+  list.classList.toggle('is-opened');
+}
+close.onclick = function () {
+  list.classList.remove('is-opened');
+}
