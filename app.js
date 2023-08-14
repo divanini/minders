49faa712cde5715d93c513779571e391ec332eff
divanini <equiptrix@outlook.com>
2023-08-14 23:54:49 +0300

    people-full+job-animation

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ menu.onclick = function () {
 close.onclick = function () {
   list.classList.remove('is-opened');
 }
+
+const wrapper = document.querySelector('.about__niche-wrapper');
+
+wrapper.addEventListener('mousemove', (e) => {
+  const { clientX, clientY } = e;
+  const { left, top, width, height } = wrapper.getBoundingClientRect();
+
+  const x = (clientX - left) / width * 100;
+  const y = (clientY - top) / height * 100;
+
+  wrapper.style.transform = `translate(${(x - 50) * 0.1}%, ${(y - 50) * 0.1}%)`;
+});
