155f1662408b0cb6fa9a5ad7b4e5871cbeb83efc
divanini <equiptrix@outlook.com>
2023-08-15 10:40:16 +0300

    places-full

--- app.js
+++ app.js
@@ -19,3 +19,29 @@ wrapper.addEventListener('mousemove', (e) => {
 
   wrapper.style.transform = `translate(${(x - 50) * 0.1}%, ${(y - 50) * 0.1}%)`;
 });
+
+
+const moreButtons = document.querySelectorAll('.info__item-more');
+const descriptions = document.querySelectorAll('.info__item-description-wrapper');
+const changeToLess = document.querySelectorAll('.info__item-more-text');
+const svgIcons = document.querySelectorAll('.info-svg');
+const infoItems = document.querySelectorAll('.info__item');
+
+moreButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    if (descriptions[index].style.display === "none" || descriptions[index].style.display === "") {
+      descriptions[index].style.display = "block";
+      changeToLess[index].textContent = 'Less';
+      svgIcons[index].setAttribute('transform', 'rotate(180)');
+      infoItems[index].style.borderColor = '#E8FE74';
+      infoItems[index].style.borderWidth = "1px";
+    } else {
+      descriptions[index].style.display = "none";
+      changeToLess[index].textContent = 'More';
+      svgIcons[index].setAttribute('transform', 'rotate(0)');
+      infoItems[index].style.borderColor = 'rgba(255, 255, 255, 0.30)';
+      infoItems[index].style.borderWidth = "1px";
+    }
+  });
+});
+;
